257c297aea398fade26e2d1fda0454ee70b4eb53
Steven <sgarcesdavila@gmail.com>
2026-05-18 22:29:40 -0500

    Remove secure demo endpoint and retain insecure cookie creation for testing

--- src/main/java/org/springframework/samples/petclinic/system/InsecureDemoController.java
+++ src/main/java/org/springframework/samples/petclinic/system/InsecureDemoController.java
@@ -1,8 +1,5 @@
 package org.springframework.samples.petclinic.system;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -12,21 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class InsecureDemoController {
 
-    @GetMapping("/secure-demo")
-    String secureDemo(@RequestParam String ownerName) throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
-
-        String query = "SELECT * FROM owners WHERE first_name = ?";
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1, ownerName);
-        statement.executeQuery();
 	@GetMapping("/insecure-cookie")
 	String insecureCookie(HttpServletResponse response) {
 		Cookie cookie = new Cookie("session_demo", "123456");
 		response.addCookie(cookie);
 
-        return "Consulta segura ejecutada";
-    }
 		return "Cookie insegura creada";
 	}
 
