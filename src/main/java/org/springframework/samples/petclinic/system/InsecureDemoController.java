c1fc9e6d6d8267a1a3cbbd068562d49083080390
Steven <sgarcesdavila@gmail.com>
2026-05-18 22:40:16 -0500

    Remove insecure cookie creation and retain password exposure endpoint

--- src/main/java/org/springframework/samples/petclinic/system/InsecureDemoController.java
+++ src/main/java/org/springframework/samples/petclinic/system/InsecureDemoController.java
@@ -1,20 +1,16 @@
 package org.springframework.samples.petclinic.system;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class InsecureDemoController {
 
-	@GetMapping("/insecure-cookie")
-	String insecureCookie(HttpServletResponse response) {
-		Cookie cookie = new Cookie("session_demo", "12345");
-		response.addCookie(cookie);
+	private static final String DATABASE_PASSWORD = "admin123456";
 
-		return "Cookie insegura creada";
+	@GetMapping("/insecure-secret")
+	String insecureSecret() {
+		return "Using password: " + DATABASE_PASSWORD;
 	}
 
 }
