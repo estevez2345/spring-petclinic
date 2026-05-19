15dc862499a121448ac54cf192e336328da5ad69
Steven <sgarcesdavila@gmail.com>
2026-05-19 08:11:02 -0500

    Refactor InsecureDemoController to remove insecure cookie endpoint and retain secure secret retrieval

--- src/main/java/org/springframework/samples/petclinic/system/InsecureDemoController.java
+++ src/main/java/org/springframework/samples/petclinic/system/InsecureDemoController.java
@@ -1,20 +1,18 @@
 package org.springframework.samples.petclinic.system;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class InsecureDemoController {
 
-	@GetMapping("/insecure-cookie")
-	String insecureCookie(HttpServletResponse response) {
-		Cookie cookie = new Cookie("session_demo", "123456");
-		response.addCookie(cookie);
+	@Value("${demo.database.password:changeme}")
+	private String databasePassword;
 
-		return "Cookie insegura creada";
+	@GetMapping("/secure-secret")
+	String secureSecret() {
+		return "Secret configured securely";
 	}
 
 }
